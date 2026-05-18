8fe7c3aaf4792780186ff66d734ce8130f6f1c29
monedales <le.moneda@outlook.com>
2026-05-17 21:25:12 -0300

    feat: centralize includes and add input error enum to header

--- include/c_plane.h
+++ include/c_plane.h
@@ -4,6 +4,9 @@
 # include <stdio.h>
 # include <stdlib.h>
 # include <string.h>
+# include <stdbool.h>
+# include <ctype.h>
+# include <errno.h>
 
 /* ************************************************************************** */
 /*                                CONSTANTS                                   */
@@ -13,6 +16,8 @@
 # define MAX_PASSENGERS 100
 # define MIN_MOMENT     1
 # define MAX_MOMENT     1000
+
+# define MAX_LINE_LEN   1024
 
 # define EXIT_OK        0
 # define EXIT_ERR       1
@@ -28,21 +33,41 @@ typedef struct s_room
 	int	*exits;
 }	t_room;
 
+typedef enum e_input_error
+{
+	NO_ERROR = 0,
+	ERR_READ_FAILURE,
+	ERR_INVALID_N,
+	ERR_INVALID_MOMENT,
+	ERR_SIZE_MISMATCH,
+	ERR_ENTRY_AFTER_EXIT,
+	ERR_NON_NUMERIC,
+	ERR_OUT_OF_RANGE,
+	ERR_MEMORY_ALLOC
+}				t_input_error;
+
 /* ************************************************************************** */
 /*                              FUNCTION PROTOTYPES                           */
 /* ************************************************************************** */
 
-/* input.c */
-int		read_input(t_room *room);
+/* input_parser.c */
+t_input_error	read_input(t_room *room);
 
 /* validation.c */
-int		validate_input(t_room *room);
+t_input_error	validate_input(t_room *room);
 
 /* algorithm.c */
-int		max_simultaneous(t_room *room);
+int				max_simultaneous(t_room *room);
 
-/* utils */
-void	free_room(t_room *room);
-void	print_error(const char *msg);
+/* utils.c */
+int				handle_error(t_input_error error);
+void			free_room(t_room *room);
+
+/* lib/ft_string.c */
+char			*ft_substr(const char *s, size_t start, size_t len);
+char			**ft_split(const char *s, char c);
+
+/* lib/ft_memory.c */
+void			ft_free_arr(char **arr, size_t until);
 
 #endif
